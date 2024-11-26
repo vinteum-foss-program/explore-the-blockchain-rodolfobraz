6bf6279d0e73ac3fe9cff5fa7ce877d86224c408
rodolfobraz <rodolfobraz@icloud.com>
2024-11-26 14:27:03 -0300

    Respostas 005, 006, 007, 008.sh

--- submission/007.sh
+++ submission/007.sh
@@ -1,1 +1,11 @@
 # Only one single output remains unspent from block 123,321. What address was it sent to?
+blockhash=$(bitcoin-cli getblockhash 123321)
+txs=$(bitcoin-cli getblock $blockhash | jq -r '.tx[]')
+
+for txid in $txs; do
+    outputs=$(bitcoin-cli listunspent | jq -r --arg txid "$txid" '.[] | select(.txid == $txid) | .address')
+    if [[ ! -z $outputs ]]; then
+        echo $outputs
+        break
+    fi
+done
--- submission/008.sh
+++ submission/008.sh
@@ -1,2 +1,6 @@
 # Which public key signed input 0 in this tx:
 #   `e5969add849689854ac7f28e45628b89f7454b83e9699e551ce14b6f90c86163`
+txid="e5969add849689854ac7f28e45628b89f7454b83e9699e551ce14b6f90c86163"
+scriptSig=$(bitcoin-cli getrawtransaction $txid true | jq -r '.vin[0].scriptSig.asm')
+pubkey=$(echo $scriptSig | awk '{print $2}')
+echo $pubkey
