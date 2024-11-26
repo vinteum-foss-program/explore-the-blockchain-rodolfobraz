a3b4cefe1c1e7d24d163d420a518efd1ea134779
rodolfobraz <rodolfobraz@icloud.com>
2024-11-26 15:18:16 -0300

    Resposta 004.sh

--- submission/004.sh
+++ submission/004.sh
@@ -1,3 +1,4 @@
 # Using descriptors, compute the taproot address at index 100 derived from this extended public key:
 #   `xpub6Cx5tvq6nACSLJdra1A6WjqTo1SgeUZRFqsX5ysEtVBMwhCCRa4kfgFqaT2o1kwL3esB1PsYr3CUdfRZYfLHJunNWUABKftK2NjHUtzDms2`
-bc1p87hwlgyj02e3fqykvkh22t4ekhxe0qn45shwc6vnrhxzr2jtqahsf63f2w
+bitcoin-cli getdescriptorinfo "tr(xpub6Cx5tvq6nACSLJdra1A6WjqTo1SgeUZRFqsX5ysEtVBMwhCCRa4kfgFqaT2o1kwL3esB1PsYr3CUdfRZYfLHJunNWUABKftK2NjHUtzDms2/0/*)"
+bitcoin-cli deriveaddresses "tr(xpub6Cx5tvq6nACSLJdra1A6WjqTo1SgeUZRFqsX5ysEtVBMwhCCRa4kfgFqaT2o1kwL3esB1PsYr3CUdfRZYfLHJunNWUABKftK2NjHUtzDms2/0/*)#5fsaz7dh" "[100,100]"
