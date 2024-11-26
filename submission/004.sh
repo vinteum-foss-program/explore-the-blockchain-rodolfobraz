7eb0db3c77d6f3eea26aaba684ad37b1097bbd4a
rodolfobraz <rodolfobraz@icloud.com>
2024-11-26 14:35:33 -0300

    Resposta 004.sh

--- submission/004.sh
+++ submission/004.sh
@@ -3,3 +3,6 @@
 desc="tr(xpub6Cx5tvq6nACSLJdra1A6WjqTo1SgeUZRFqsX5ysEtVBMwhCCRa4kfgFqaT2o1kwL3esB1PsYr3CUdfRZYfLHJunNWUABKftK2NjHUtzDms2/100)"
 address=$(bitcoin-cli getdescriptorinfo "$desc" | jq -r '.descriptor')
 bitcoin-cli deriveaddresses "$address"
+[
+  "bc1p3yrtpvv6czx63h2sxwmeep8q98h94w4288fc4cvrkqephalydfgszgacf9"
+]
