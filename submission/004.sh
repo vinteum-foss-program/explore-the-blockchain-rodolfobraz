ca0b0c473fef3c01802d2cfbd896028624adaedc
rodolfobraz <rodolfobraz@icloud.com>
2024-11-27 09:11:55 -0300

    Resposta 004.sh

--- submission/004.sh
+++ submission/004.sh
@@ -1,15 +1,14 @@
 # Using descriptors, compute the taproot address at index 100 derived from this extended public key:
 #   `xpub6Cx5tvq6nACSLJdra1A6WjqTo1SgeUZRFqsX5ysEtVBMwhCCRa4kfgFqaT2o1kwL3esB1PsYr3CUdfRZYfLHJunNWUABKftK2NjHUtzDms2`
 # Gerar o checksum do descritor
-CHECKSUM_DESCRIPTOR=$(bitcoin-cli getdescriptorinfo "tr(${XPUB}/0/100/*)" | jq -r '.descriptor')
 
-# Derivar o endereço
-ADDRESS=$(bitcoin-cli deriveaddresses "$CHECKSUM_DESCRIPTOR" 0)
+#!/bin/bash
 
-# Remover aspas e brackets
-ADDRESS=$(echo "$ADDRESS" | tr -d '[]"')
+# Descriptor com checksum
+descriptor_with_checksum=$(bitcoin-cli getdescriptorinfo "tr(xpub6Cx5tvq6nACSLJdra1A6WjqTo1SgeUZRFqsX5ysEtVBMwhCCRa4kfgFqaT2o1kwL3esB1PsYr3CUdfRZYfLHJunNWUABKftK2NjHUtzDms2/0/100)" | jq -r '.descriptor')
 
-echo "Taproot Address at index 100: $ADDRESS"
+# Derivando endereço
+address=$(bitcoin-cli deriveaddresses "$descriptor_with_checksum")
 
-Taproot Address at index 100:
-  bc1pn38ddgas68jszc6m53act04a6jh9a4klnmsrcvz95ngj5mcv64fs4ehzd5
+# Exibir endereço
+echo "$address"
