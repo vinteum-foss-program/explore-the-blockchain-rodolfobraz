073ca3c6fafc3241be4ec49034d327d04906ef9e
rodolfobraz <rodolfobraz@icloud.com>
2024-12-01 23:46:30 -0300

    Resposta 008

--- submission/008.sh
+++ submission/008.sh
@@ -1,7 +1,7 @@
 # Which public key signed input 0 in this tx:
 #   `e5969add849689854ac7f28e45628b89f7454b83e9699e551ce14b6f90c86163`
  
-xid='e5969add849689854ac7f28e45628b89f7454b83e9699e551ce14b6f90c86163'
+txid='e5969add849689854ac7f28e45628b89f7454b83e9699e551ce14b6f90c86163'
 
 tx=$(bitcoin-cli getrawtransaction $txid true)
 
