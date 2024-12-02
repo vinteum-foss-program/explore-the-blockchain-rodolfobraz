6be1ae6f255ece233a71008b6d96a3f5c946603d
rodolfobraz <rodolfobraz@icloud.com>
2024-12-01 22:41:16 -0300

    Resposta 007

--- submission/007.sh
+++ submission/007.sh
@@ -1,36 +1,15 @@
-#!/bin/bash
+blockhash=$(bitcoin-cli getblockhash 123321)
+blockdata=$(bitcoin-cli getblock "$blockhash" 1)
+txids=$(echo "$blockdata" | jq -r '.tx[]')
 
-# Obtém o hash do bloco 123321
-block_hash=$(bitcoin-cli getblockhash 123321)
-
-# Obtém os dados completos do bloco com detalhes das transações
-block_data=$(bitcoin-cli getblock "$block_hash" 2)
-
-# Percorre cada transação no bloco
-echo "$block_data" | jq -c '.tx[]' | while read tx; do
-    txid=$(echo "$tx" | jq -r '.txid')
-    
-    # Percorre cada saída (vout) da transação
-    echo "$tx" | jq -c '.vout[]' | while read vout; do
-        n=$(echo "$vout" | jq '.n')
-        
-        # Verifica se a saída ainda não foi gasta
-        utxo=$(bitcoin-cli gettxout "$txid" "$n")
-        if [ -n "$utxo" ]; then
-            # Obtém o endereço associado à saída
-            address=$(echo "$vout" | jq -r '.scriptPubKey.addresses[0] // .scriptPubKey.address // empty')
-            if [ -n "$address" ]; then
-                echo "Saída não gasta encontrada:"
-                echo "TXID: $txid"
-                echo "VOUT: $n"
-                echo "Endereço: $address"
-                echo "-----------------------------"
-            else
-                echo "Saída não gasta sem endereço reconhecido:"
-                echo "TXID: $txid"
-                echo "VOUT: $n"
-                echo "-----------------------------"
-            fi
+for txid in $txids; do
+    txdata=$(bitcoin-cli getrawtransaction "$txid" 1)
+    num_vout=$(echo "$txdata" | jq '.vout | length')
+    for (( vout=0; vout<num_vout; vout++ )); do
+        txout=$(bitcoin-cli gettxout "$txid" $vout)
+        if [ "$txout" != "null" ]; then
+            address=$(echo "$txdata" | jq -r ".vout[$vout].scriptPubKey.addresses[0]")
+            echo "O output não gasto está na transação $txid, vout $vout, enviado para o endereço $address"
         fi
     done
 done
