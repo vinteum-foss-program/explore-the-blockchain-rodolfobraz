acef52c73ba76f1653faf3e0bb0447a67217e464
rodolfobraz <rodolfobraz@icloud.com>
2024-12-01 22:58:50 -0300

    Resposta 008

--- submission/008.sh
+++ submission/008.sh
@@ -1,19 +1,22 @@
 # Which public key signed input 0 in this tx:
 #   `e5969add849689854ac7f28e45628b89f7454b83e9699e551ce14b6f90c86163`
+ 
+# ID da transação
+txid='e5969add849689854ac7f28e45628b89f7454b83e9699e551ce14b6f90c86163'
 
-#!/bin/bash
-
-# Transaction ID
-txid='e5969add849689854ac7ec6501d018d322334f142c7c11aa24b9cffec03161eca35a1e32a71f'
-
-# Retrieve and decode the transaction
+# Recupera e decodifica a transação
 tx=$(bitcoin-cli getrawtransaction $txid true)
 
-# Extract the 'asm' field of 'scriptSig' in input 0
+# Extrai o campo 'asm' do 'scriptSig' da entrada 0
 asm=$(echo "$tx" | jq -r '.vin[0].scriptSig.asm')
 
-# Extract the public key
-pubkey=$(echo $asm | cut -d' ' -f2)
+if [ -z "$asm" ] || [ "$asm" == "null" ]; then
+  # Se 'asm' está vazio, é uma transação SegWit; extrai a chave pública dos dados de testemunha
+  pubkey=$(echo "$tx" | jq -r '.vin[0].txinwitness[-1]')
+else
+  # Extrai a chave pública do 'asm'
+  pubkey=$(echo $asm | cut -d' ' -f2)
+fi
 
-# Display the public key
+# Exibe a chave pública
 echo $pubkey
