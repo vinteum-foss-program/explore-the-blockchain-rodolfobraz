1fa2b098ab858afff1cbc9f46d3b53c826346126
rodolfobraz <rodolfobraz@icloud.com>
2024-12-01 23:02:40 -0300

    Resposta 008

--- submission/008.sh
+++ submission/008.sh
@@ -1,21 +1,29 @@
 # Which public key signed input 0 in this tx:
 #   `e5969add849689854ac7f28e45628b89f7454b83e9699e551ce14b6f90c86163`
  
+
 # ID da transação
 txid='e5969add849689854ac7f28e45628b89f7454b83e9699e551ce14b6f90c86163'
 
 # Recupera e decodifica a transação
 tx=$(bitcoin-cli getrawtransaction $txid true)
 
-# Extrai o campo 'asm' do 'scriptSig' da entrada 0
-asm=$(echo "$tx" | jq -r '.vin[0].scriptSig.asm')
+# Verifica se 'txinwitness' existe na entrada 0
+witness=$(echo "$tx" | jq '.vin[0].txinwitness')
 
-if [ -z "$asm" ] || [ "$asm" == "null" ]; then
-  # Se 'asm' está vazio, é uma transação SegWit; extrai a chave pública dos dados de testemunha
-  pubkey=$(echo "$tx" | jq -r '.vin[0].txinwitness[-1]')
+if [ "$witness" != "null" ]; then
+  # É uma transação SegWit; extrai a chave pública de 'txinwitness[1]'
+  pubkey=$(echo "$tx" | jq -r '.vin[0].txinwitness[1]')
 else
-  # Extrai a chave pública do 'asm'
-  pubkey=$(echo $asm | cut -d' ' -f2)
+  # Extrai o campo 'asm' do 'scriptSig' na entrada 0
+  asm=$(echo "$tx" | jq -r '.vin[0].scriptSig.asm')
+  if [ -z "$asm" ] || [ "$asm" == "null" ]; then
+    echo "Nenhum 'scriptSig' encontrado na entrada 0."
+    exit 1
+  else
+    # Extrai a chave pública do 'asm'
+    pubkey=$(echo "$asm" | cut -d' ' -f2)
+  fi
 fi
 
 # Exibe a chave pública
