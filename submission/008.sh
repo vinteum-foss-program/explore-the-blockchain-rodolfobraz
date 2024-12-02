eb38e582d723777ba0bee47f934ec9265f0378cd
rodolfobraz <rodolfobraz@icloud.com>
2024-12-01 22:49:09 -0300

    Resposta 008

--- submission/008.sh
+++ submission/008.sh
@@ -1,8 +1,21 @@
 # Which public key signed input 0 in this tx:
 #   `e5969add849689854ac7f28e45628b89f7454b83e9699e551ce14b6f90c86163`
-txid="e5969add849689854ac7f28e45628b89f7454b83e9699e551ce14b6f90c86163"
-scriptSig=$(bitcoin-cli getrawtransaction $txid true | jq -r '.vin[0].scriptSig.asm')
-pubkey=$(echo $scriptSig | awk '{print $2}')
+#!/bin/bash
+
+# Transaction ID
+txid='e5969add849689854ac7f28e45628b89f7454b83e9699e551ce14b6f90c86163'
+
+# Retrieve and decode the transaction
+tx=$(bitcoin-cli getrawtransaction $txid true)
+
+# Extract the 'asm' field of 'scriptSig' in input 0
+asm=$(echo "$tx" | jq -r '.vin[0].scriptSig.asm')
+
+# Extract the public key
+pubkey=$(echo $asm | cut -d' ' -f2)
+
+# Display the public key
+echo "Public Key for input 0:"
 echo $pubkey
 
 
