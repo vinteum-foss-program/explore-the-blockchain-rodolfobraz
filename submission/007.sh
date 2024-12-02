2e78934fe7607ba0ef1d55ea9d9a6d36aab5974d
rodolfobraz <rodolfobraz@icloud.com>
2024-12-01 21:51:24 -0300

    Resposta 007.sh

--- submission/007.sh
+++ submission/007.sh
@@ -1,28 +1,28 @@
-# Passo 1: Obter o hash do bloco
+#!/bin/bash
+
+# Obter o hash do bloco
 blockhash=$(bitcoin-cli getblockhash 123321)
 
-# Passo 2: Listar todas as transações do bloco
+# Listar todas as transações do bloco
 txs=$(bitcoin-cli getblock "$blockhash" | jq -r '.tx[]')
 
-# Passo 3: Loop através das transações para encontrar o UTXO
+# Loop através das transações para encontrar o UTXO
 for txid in $txs; do
     # Decodificar a transação para extrair os vouts
     raw_tx=$(bitcoin-cli getrawtransaction "$txid")
     decoded_tx=$(bitcoin-cli decoderawtransaction "$raw_tx")
     
-    # Iterar sobre os vouts da transação
     for index in $(echo "$decoded_tx" | jq -r '.vout[].n'); do
         # Verificar se o vout foi gasto usando gettxout
         utxo=$(bitcoin-cli gettxout "$txid" "$index")
         if [ -n "$utxo" ]; then
             # Se o gettxout retorna algo, significa que o vout não foi gasto
             address=$(echo "$utxo" | jq -r '.scriptPubKey.addresses[0]')
-            echo "UTXO encontrado:"
-            echo "Endereço: $address"
-            echo "Valor: $(echo "$utxo" | jq -r '.value') BTC"
+            echo "$address"
             exit 0
         fi
     done
 done
 
-echo "Nenhum UTXO não gasto encontrado."
+# Caso nenhum UTXO seja encontrado
+exit 1
